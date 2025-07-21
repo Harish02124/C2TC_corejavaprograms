e5cd98c90ff113bcf9ba4d3226d7748ab6f6844e
STUDENT <STUDENT@DESKTOP-7E2QAJA>
2025-07-21 16:01:17 +0530

    new

--- C2TC_corejavaprograms/src/day1/entity/Customer.java
+++ C2TC_corejavaprograms/src/day1/entity/Customer.java
@@ -1,8 +1,30 @@
 package day1.entity;
 
 public class Customer {
-	int cid;
-	String cname;
-	String ccity;
-
+	private int cid;
+	private String cname;
+	private String ccity;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getCcity() {
+		return ccity;
+	}
+	public void setCcity(String ccity) {
+		this.ccity = ccity;
+	}
+	@Override
+	public String toString() {
+		return "Customer [cid=" + cid + ", cname=" + cname + ", ccity=" + ccity + "]";
+	}
+	
 }
--- C2TC_corejavaprograms/src/day1/entity/CustomerDemo.java
+++ C2TC_corejavaprograms/src/day1/entity/CustomerDemo.java
@@ -5,12 +5,10 @@ public class CustomerDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          Customer c1=new Customer();
-         c1.cid=112;
-         c1.cname="raj";
-         c1.ccity="pondy";
-         System.out.println("Customer id:"+c1.cid);
-         System.out.println("Customer name:"+c1.cname);
-         System.out.println("Customer city:"+c1.ccity);
+         c1.setCid(112);
+         c1.setCname("raj");
+         c1.setCcity("pondy");
+         System.out.println(c1);
 	}
 
 }
