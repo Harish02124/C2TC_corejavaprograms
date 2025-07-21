40a2b88453d964ab66cd2206992a77c1d96c6fca
STUDENT <STUDENT@DESKTOP-7E2QAJA>
2025-07-21 14:38:16 +0530

    new

---
+++ C2TC_corejavaprograms/src/day1/basics/Java_02.java
@@ -0,0 +1,22 @@
+package day1.basics;
+
+public class Java_02 {
+	public static void main()
+	{
+	 System.out.println("welcome to TFs...");
+	}
+	public static void main(char c[])
+	{
+	 System.out.println("Welcome form main2");
+	}
+	public void main(String ar)
+	{
+		System.out.println("welcome form main3");
+	}
+	public static void main (String[] args) 
+	{
+		System.out.println("welcome to jfs..");
+		System.out.println("next time");
+	}
+
+}
