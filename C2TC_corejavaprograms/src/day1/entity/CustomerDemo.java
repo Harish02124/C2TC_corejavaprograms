7df03609e8f92becf4246bd893a860105371b2ed
STUDENT <STUDENT@DESKTOP-7E2QAJA>
2025-07-21 15:40:16 +0530

    new

---
+++ C2TC_corejavaprograms/src/day1/entity/CustomerDemo.java
@@ -0,0 +1,16 @@
+package day1.entity;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Customer c1=new Customer();
+         c1.cid=112;
+         c1.cname="raj";
+         c1.ccity="pondy";
+         System.out.println("Customer id:"+c1.cid);
+         System.out.println("Customer name:"+c1.cname);
+         System.out.println("Customer city:"+c1.ccity);
+	}
+
+}
